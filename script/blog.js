2a5c3f132f76073f62cb6c5d3a77ed4ec8957ffd
Rishabh Singh <itsindrajput@gmail.com>
2024-04-12 13:35:03 +0530

    Blog Post Slider

--- script/blog.js
+++ script/blog.js
@@ -9,3 +9,30 @@ hoverElement.addEventListener("mouseout", function () {
 selectedElement.addEventListener("mouseout", function () {
   selectedElement.style.display = "none";
 });
+
+// JS For Latest Blog Post Begins Here
+const slider = document.getElementById("slider");
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+let currentSlide = 0;
+
+function updateSlider() {
+  const translateValue = -currentSlide * 100 + "%";
+  slider.style.transform = "translateX(" + translateValue + ")";
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  updateSlider();
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  updateSlider();
+}
+
+// Button click events
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
